435ec8ff3977a7e4fdfd7be86159bac67fa4813e
Jag-kr <jagjeetk@iitbhilai.ac.in>
2026-06-01 14:19:59 +0530

    refactor App component to use class-based state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from "react";
+import { Component } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
 
-  return (
-    <>
-      <header>
-        <h1>Counter App</h1>
-      </header>
-      <section>
-        Count: {count}
-        <div className="counter-container">
-          <button
-            type="button"
-            className="counter"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            Increment
-          </button>
-          <button
-            type="button"
-            className="counter"
-            onClick={() => setCount((count) => count - 1)}
-          >
-            Decrement
-          </button>
-        </div>
-      </section>
-    </>
-  );
+  increment() {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  }
+
+  decrement() {
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
+  }
+
+  render() {
+    const { count } = this.state;
+    return (
+      <>
+        <header>
+          <h1>Counter App</h1>
+        </header>
+        <section>
+          Count: {count}
+          <div className="counter-container">
+            <button
+              type="button"
+              className="counter"
+              onClick={() => this.increment()}
+            >
+              Increment
+            </button>
+            <button
+              type="button"
+              className="counter"
+              onClick={() => this.decrement()}
+            >
+              Decrement
+            </button>
+          </div>
+        </section>
+      </>
+    );
+  }
 }
 
 export default App;
